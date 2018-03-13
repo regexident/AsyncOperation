fe3d9794715f2aeebc49b3305d74c2a9447483f9
Vincent Esche <regexident@gmail.com>
2018-03-13 16:59:07 +0100

    Improved indentation

--- AsyncOperation/Classes/AsyncOperation.swift
+++ AsyncOperation/Classes/AsyncOperation.swift
@@ -8,98 +8,95 @@
 
 import Foundation
 
-open class AsyncOperation : Operation {
+open class AsyncOperation: Operation {
+    public enum State: String {
+        case waiting = "isWaiting"
+        case ready = "isReady"
+        case executing = "isExecuting"
+        case finished = "isFinished"
+        case cancelled = "isCancelled"
+    }
 
-	public enum State : String {
-		case waiting = "isWaiting"
-		case ready = "isReady"
-		case executing = "isExecuting"
-		case finished = "isFinished"
-		case cancelled = "isCancelled"
-	}
-
-	open var state: State = State.waiting {
-		willSet {
-			willChangeValue(forKey: State.ready.rawValue)
-			willChangeValue(forKey: State.executing.rawValue)
-			willChangeValue(forKey: State.finished.rawValue)
-			willChangeValue(forKey: State.cancelled.rawValue)
-		}
-		didSet {
+    open var state: State = State.waiting {
+        willSet {
+            willChangeValue(forKey: State.ready.rawValue)
+            willChangeValue(forKey: State.executing.rawValue)
+            willChangeValue(forKey: State.finished.rawValue)
+            willChangeValue(forKey: State.cancelled.rawValue)
+        }
+        didSet {
             switch self.state {
             case .waiting:
-                assert(oldValue == .waiting)
+                assert(oldValue == .waiting, "Invalid change from \(oldValue) to \(self.state)")
             case .ready:
-                assert(oldValue == .waiting)
+                assert(oldValue == .waiting, "Invalid change from \(oldValue) to \(self.state)")
             case .executing:
-                assert(oldValue == .ready || oldValue == .waiting)
+                assert(
+                    oldValue == .ready || oldValue == .waiting,
+                    "Invalid change from \(oldValue) to \(self.state)"
+                )
             case .finished:
-                assert(oldValue != .cancelled)
+                assert(oldValue != .cancelled, "Invalid change from \(oldValue) to \(self.state)")
             case .cancelled:
                 break
             }
-            
-			didChangeValue(forKey: State.cancelled.rawValue)
-			didChangeValue(forKey: State.finished.rawValue)
-			didChangeValue(forKey: State.executing.rawValue)
-			didChangeValue(forKey: State.ready.rawValue)
-		}
-	}
 
-	open override var isReady: Bool {
-		if self.state == .waiting {
-			return super.isReady
-		} else {
-			return self.state == .ready
-		}
-	}
+            didChangeValue(forKey: State.cancelled.rawValue)
+            didChangeValue(forKey: State.finished.rawValue)
+            didChangeValue(forKey: State.executing.rawValue)
+            didChangeValue(forKey: State.ready.rawValue)
+        }
+    }
 
-	open override var isExecuting: Bool {
+    open override var isReady: Bool {
+        if self.state == .waiting {
+            return super.isReady
+        } else {
+            return self.state == .ready
+        }
+    }
 
-		if self.state == .waiting {
-			return super.isExecuting
-		} else {
-			return self.state == .executing
-		}
-	}
+    open override var isExecuting: Bool {
+        if self.state == .waiting {
+            return super.isExecuting
+        } else {
+            return self.state == .executing
+        }
+    }
 
-	open override var isFinished: Bool {
+    open override var isFinished: Bool {
+        if self.state == .waiting {
+            return super.isFinished
+        } else {
+            return self.state == .finished
+        }
+    }
 
-		if self.state == .waiting {
-			return super.isFinished
-		} else {
-			return self.state == .finished
-		}
-	}
+    open override var isCancelled: Bool {
+        if self.state == .waiting {
+            return super.isCancelled
+        } else {
+            return self.state == .cancelled
+        }
+    }
 
-	open override var isCancelled: Bool {
-		if self.state == .waiting {
-			return super.isCancelled
-		} else {
-			return self.state == .cancelled
-		}
-	}
-
-	open override var isAsynchronous: Bool {
-		return true
-	}
-
+    open override var isAsynchronous: Bool {
+        return true
+    }
 }
 
-open class AsyncBlockOperation : AsyncOperation {
+open class AsyncBlockOperation: AsyncOperation {
+    public typealias Closure = (AsyncBlockOperation) -> ()
 
-	public typealias Closure = (AsyncBlockOperation) -> ()
+    let closure: Closure
 
-	let closure: Closure
+    public init(closure: @escaping Closure) {
+        self.closure = closure
+    }
 
-	public init(closure: @escaping Closure) {
-		self.closure = closure
-	}
-
-	open override func main() {
+    open override func main() {
         guard !self.isCancelled else { return }
-        
-		self.closure(self)
-	}
 
+        self.closure(self)
+    }
 }
